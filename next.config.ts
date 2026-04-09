5f9d725e83397ff4e1d5979824a43fd0b76c017b
Manuel Valdés <mvaldes86@gmail.com>
2026-04-09 16:59:50 -0400

    Config: Optimize for Hostinger stability

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  staticPageGenerationTimeout: 180,
 };
+
 
 export default nextConfig;
