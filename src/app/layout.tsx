80ac299d171b402617b665d9f610b4f45f5f4340
Manuel Valdés <mvaldes86@gmail.com>
2026-04-10 21:30:56 -0400

    Emergency: Revert Meta Pixel to fix production hydration crash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Outfit, Inter } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const outfit = Outfit({
@@ -36,31 +35,7 @@ export default function RootLayout({
       lang="es"
       className={`${outfit.variable} ${inter.variable} h-full antialiased`}
     >
-      <head>
-        <Script id="facebook-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1428411498841708');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-      </head>
       <body className="min-h-full flex flex-col bg-[#0A0A1F] text-white selection:bg-primary/30">
-        <noscript>
-          <img 
-            height="1" 
-            width="1" 
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1428411498841708&ev=PageView&noscript=1"
-          />
-        </noscript>
         {children}
         
         {/* Botón WhatsApp Flotante */}
@@ -82,5 +57,6 @@ export default function RootLayout({
     </html>
   );
 }
+
 
 
